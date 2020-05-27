4852fd403ae1698ed2514f44b7b1aee857dd9fc5
sohaib <sohaib.code@gmail.com>
2020-05-27 12:44:17 +0000

    fix root reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
-import { persistedReducer } from "./root-reducer";
+import { persistedReducer } from "./rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const middlewares = [logger];
